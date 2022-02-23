8f037bb61cf7a04ba6339d38ff4b57dacee24e47
Duncan Tyler <Sylence@users.noreply.github.com>
2022-02-23 15:49:11 +1300

    Base files added for header loading example

---
+++ JS/code.js
@@ -0,0 +1,67 @@
+function changeHTML(){
+    var text = document.getElementById("firstName").value;
+    
+    
+    
+    var number = Number(document.getElementById("theNumber").value);
+    // test that the number is between 10 and 30
+    if (number < 10 || number > 30) {
+        alert("OI! Idiot! Put a number between 10 and 30. GOSH!");
+        return;
+    }
+    
+    document.getElementById("changeMe").innerHTML = number + number;
+}
+
+function toggleText() {
+    $("p").toggle(1000);
+}
+
+function addParagraph() {
+    $("#firstLine").append("<p>Hello, I'm a new P tag! :D XD</p>");
+}
+
+
+
+function addThings() {
+    $("#box").append("<p>Hello! I am a P tag! :D XD</p>")
+}
+
+
+
+// Uses Jquery to iterate over the items of a list and print each
+// to the console
+function printList() {
+    $("li").each(function (index) {
+        console.log(index + ": " + $(this).text());
+    });
+}
+
+$("#header").load("header.html");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
